48783f0d551962a46fbdd55e7b767a86b6381636
mayconfidelis <mayconfidelis@gmail.com>
2025-02-27 22:23:58 -0300

    Método para verificar se um nÃúmero é par

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    public static boolean ehPar(int numero){
+        return numero % 2 == 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean resultado = ehPar(5);
+        System.out.println("O número é par? " + resultado);
     }
 }
