7cd9e32ef48a4035ed5d980d5659bc5c58fa67a1
mayconfidelis <mayconfidelis@gmail.com>
2025-02-27 22:20:18 -0300

    Método para resto de divisão

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    public static int restoDivisao(int a, int b) {
+        return a % b;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int resultado = restoDivisao(4, 5);
+        System.out.println("Resto da divisão: " + resultado);
     }
 }
