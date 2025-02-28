59049d04ee6f5f9932411d2afe83c49dbe4d3430
mayconfidelis <mayconfidelis@gmail.com>
2025-02-27 22:13:40 -0300

    Método para divisão

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    public static double divisao (int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Divisão por zero não é permitida!");
+        }
+        return (double) a / b;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double resultado = divisao(10, 3);
+        System.out.println("Resultado da divisão: " + resultado);
     }
 }
